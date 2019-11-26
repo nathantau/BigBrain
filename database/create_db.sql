105e7b25a60217456e63792e61cf8e2a4ea24e8c
Nathan Au <nathan.au1280@gmail.com>
2019-11-25 23:36:02 -0500

    Finished changes to allow for docker compose func.

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,1 +1,5 @@
-CREATE DATABASE bigbrain;
+-- CREATE DATABASE bigbrain;
+\c bigbrain
+CREATE TABLE users(email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, salt VARCHAR(255) NOT NULL);
+INSERT INTO users(email, password, salt) VALUES ('rando', 'rando', 'rando');
+SELECT * FROM users;
